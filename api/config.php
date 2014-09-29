3ed8f7fab90d40b8887a1ad2273ea766a9977f7e
ahmedhawas <ahmedhawas7@gmail.com>
2014-09-29 18:25:32 -0400

    initial commit

---
+++ api/config.php
@@ -0,0 +1,9 @@
+<?php
+    // Consumer Key
+    define('CONSUMER_KEY', 'L7kPeSuziKrEvoa7N0IWiSO74');
+    define('CONSUMER_SECRET', '5QIRxDDx3yOsEtdJ0qdAwEQfE3NlPrQm3RFUoRyPrU0LF6HM7b');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '323014024-OiVHoOaV7t3lZapbXmzwnjBdgdGwdGirSFU8BgJG');
+    define('ACCESS_SECRET', 'IFhj1E9g75Zbrtc2jLbkvcOF76znmxuMGFHvQSSHL6jX7');
+?>
